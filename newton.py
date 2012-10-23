73a7d8205f2a159c8995311d086cdba1bcba80ec
Seth Davidovits <sdavidov@princeton.edu>
2012-10-23 17:22:24 -0400

    Added code to enable Newton to take an analytic jacobian, ran tests to make sure that it defaults to approximate one correctly

--- newton.py
+++ newton.py
@@ -7,7 +7,7 @@ import numpy as N
 import functions as F
 
 class Newton(object):
-    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6):
+    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6, analyticDF=None):
         """Return a new object to find roots of f(x) = 0 using Newton's method.
         tol:     tolerance for iteration (iterate until |f(x)| < tol)
         maxiter: maximum number of iterations to perform
@@ -16,6 +16,7 @@ class Newton(object):
         self._tol = tol
         self._maxiter = maxiter
         self._dx = dx
+        self._analyticDF = analyticDF
 
     def solve(self, x0):
         """Return a root of f(x) = 0, using Newton's method, starting from
@@ -26,7 +27,7 @@ class Newton(object):
             if N.linalg.norm(fx) < self._tol:
                 return x
             x = self.step(x, fx)
-	    
+        
         if N.linalg.norm(fx) < self._tol:
             return x
         else:
@@ -37,6 +38,10 @@ class Newton(object):
         If the argument fx is provided, assumes fx = f(x)"""
         if fx is None:
             fx = self._f(x)
-        Df_x = F.ApproximateJacobian(self._f, x, self._dx)
+        if self._analyticDF is None:        
+            Df_x = F.ApproximateJacobian(self._f, x, self._dx)
+        else:
+    	    Df_x = self._analyticDF(x)
+	    
         h = N.linalg.solve(N.matrix(Df_x), N.matrix(fx))
         return x - h
