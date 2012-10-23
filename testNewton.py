305a34e7b863408637b753bf257f0dd1f0b65457
Seth Davidovits <sdavidov@princeton.edu>
2012-10-23 18:08:39 -0400

    Tests for newton breaking with big dx, test of analytic jacobian (with big dx to make sure it's actually being used), both work

--- testNewton.py
+++ testNewton.py
@@ -19,6 +19,25 @@ class TestNewton(unittest.TestCase):
         x = solver.solve(x0)
         N.testing.assert_array_almost_equal(x, xRoot)
 
+    def testNonlinear2DBreaksWithBigDx(self):
+        solver = newton.Newton(F.TwoDNonlinear, tol=1.e-12, maxiter=10, dx=1.)
+        x0 = N.matrix([[1.],[2.]])
+        xRoot = N.matrix([[0.826031357654187],[0.563624162161259]])        
+        try:            
+            x = solver.solve(x0)
+        except Exception as inst:
+            message, curRoot = inst.args
+            validmessage = 'Newton failed to converge with given number of max iterations'
+            self.assertEqual(message,validmessage)            
+            assert(N.linalg.norm(curRoot - xRoot) > 0.5*10**(-3))
+
+    def testNonlinear2DExact(self):
+        solver = newton.Newton(F.TwoDNonlinear, tol=1.e-12, maxiter=10, dx=1., analyticDF=F.TwoDNonlinearExactJacobian)
+        x0 = N.matrix([[1.],[2.]])
+        xRoot = N.matrix([[0.826031357654187],[0.563624162161259]])        
+        x = solver.solve(x0)
+        N.testing.assert_array_almost_equal(x, xRoot)
+
     def testNonlinear1D(self):
         poly = F.Polynomial([1,2,-10,2])
         solver = newton.Newton(poly, tol=1.e-12, maxiter=15)
@@ -32,7 +51,7 @@ class TestNewton(unittest.TestCase):
         try:            
             x = solver.solve(0.1)
         except Exception as inst:
-            message, = inst.args
+            message, extra = inst.args
             validmessage = 'Newton failed to converge with given number of max iterations'
             self.assertEqual(message,validmessage)            
         
