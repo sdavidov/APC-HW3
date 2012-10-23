75199f9e5ca6d3341d76d8b4fc6537653bcd6d3b
Seth Davidovits <sdavidov@princeton.edu>
2012-10-23 01:45:01 -0400

    Added nonlinear 2D test of Newton method, passes

--- testNewton.py
+++ testNewton.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import newton
+import functions as F
 import unittest
 import numpy as N
 
@@ -10,6 +11,13 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
+
+    def testNonlinear2D(self):
+        solver = newton.Newton(F.TwoDNonlinear, tol=1.e-12, maxiter=10)
+        x0 = N.matrix([[1.],[2.]])
+        xRoot = N.matrix([[0.826031357654187],[0.563624162161259]])        
+        x = solver.solve(x0)
+        N.testing.assert_array_almost_equal(x, xRoot)
 
 if __name__ == "__main__":
     unittest.main()
